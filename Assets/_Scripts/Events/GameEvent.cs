54e6cd3d9e5ac412e79f5fac2415b5b48d345348
kroonhorstdino <kroonhorstdino@hotmail.de>
2020-01-11 00:33:43 +0100

    Added custom event system (closes #13)
    Added custom event handler using Action<GameEvent>

---
+++ Assets/_Scripts/Events/GameEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RocketRaptor.Events;
+using UnityEngine;
+
+namespace RocketRaptor.Events
+{
+
+    public enum GameEvents
+    {
+        Default
+    }
+
+    public interface IGameEvent
+    {
+    }
+
+    public class GameEventBase : IGameEvent
+    {
+        public readonly IEventObserver sender;
+        public readonly float time;
+
+        public GameEventBase(IEventObserver sender)
+        {
+            this.sender = sender;
+            this.time = Time.time;
+        }
+    }
+
+    public class DamageEvent : GameEventBase
+    {
+        public readonly EntityBase target;
+        public readonly float damage;
+
+        public DamageEvent(IEventObserver sender, EntityBase target) : base(sender)
+        {
+            this.target = target;
+        }
+    }
+
+    public class DeathEvent : GameEventBase
+    {
+        public DeathEvent(IEventObserver sender) : base(sender)
+        {
+        }
+    }
+
+}
---
+++ Assets/_Scripts/Events/GameEventHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RocketRaptor.Events
+{
+
+    public class GameEventHandler : MonoBehaviour
+    {
+        Dictionary<GameEvents, List<Action<IGameEvent>>> listeners;
+
+
+        [SerializeField] int _numEvt;
+
+        private void Awake()
+        {
+            listeners = new Dictionary<GameEvents, List<Action<IGameEvent>>>();
+        }
+
+        private void Update()
+        {
+            _numEvt = listeners.Count;
+        }
+
+        /// <summary>
+        /// Add action to listeners 
+        /// </summary>
+        /// <param name="listener"></param>
+        /// <param name="eventName"></param>
+        public void AddListener(Action<IGameEvent> listener, GameEvents eventName)
+        {
+            //When this kind of game event has not yet been subscribed, generate a new list.
+            if (!listeners.ContainsKey(eventName))
+            {
+                listeners.Add(eventName, new List<Action<IGameEvent>>());
+            }
+
+            try
+            {
+                //Add listener to list of listeners for this event
+                listeners[eventName].Add(listener);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        /// <summary>
+        /// Remove action from listeners
+        /// </summary>
+        /// <param name="listener"></param>
+        public void RemoveListener(Action<IGameEvent> listener)
+        {
+            //Go through each list and delete listener if found
+            foreach (GameEvents eventName in listeners.Keys)
+            {
+                //If desired action is found, remove and break
+                if (listeners[eventName].Remove(listener))
+                {
+                    if (listeners[eventName].Count == 0)
+                    {
+                        listeners.Remove(eventName);
+                    }
+                    else
+                    {
+                        //TODO: Neccessary? Correct?
+                        listeners[eventName].TrimExcess();
+                    }
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// TODO: Remove all listeners of one object
+        /// </summary>
+        public void RemoveListenerOfObserver(IEventObserver observer)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="gameEvent"></param>
+        public void Trigger(GameEvents eventName, IGameEvent gameEvent)
+        {
+            List<Action<IGameEvent>> eventListeners;
+
+            if (listeners.TryGetValue(eventName, out eventListeners))
+            {
+                foreach (Action<IGameEvent> listener in eventListeners)
+                {
+                    listener.Invoke(gameEvent);
+                }
+            }
+            else
+            {
+                //Do nothing
+            }
+        }
+    }
+
+    public interface IEventObserver
+    {
+    }
+
+}
